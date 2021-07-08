5e7a69873125f02240e60c0260006de6060ad61b
Leandro <fahurleandro@gmail.com>
2021-07-08 15:24:12 -0700

    first step

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Card, CardGrid, Container, Header } from './Elements';
 import './App.css';
+import Menu from './Menu';
+import blue from './blue.png';
+import purp from './purp.png';
+import black from './black.png';
+import green from './green.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div
+      initial={{
+        opacity: 0,
+        x: 100,
+        y: 100,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        y: 0,
+      }}
+      transition={{ duration: 1 }}
+    >
+      <Header>
+        <Menu />
+        <h1>Header</h1>
+      </Header>
+      <Container>
+        <h2>Super Cool</h2>
+        <CardGrid>
+          <Card style={{ background: 'var(--purp)' }}>
+            <h3>Some card</h3>
+            <img src={purp} />
+          </Card>
+          <Card style={{ background: 'var(--blue)' }}>
+            <h3>Some card</h3>
+            <img src={blue} />
+          </Card>
+          <Card style={{ background: 'var(--black)' }}>
+            <h3>Some card</h3>
+            <img src={black} />
+          </Card>
+          <Card style={{ background: 'var(--green)' }}>
+            <h3>Some card</h3>
+            <img src={green} />
+          </Card>
+        </CardGrid>
+      </Container>
+    </motion.div>
   );
 }
 
