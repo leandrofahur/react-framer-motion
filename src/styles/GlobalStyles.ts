08a061b5b7a446bff084d54c0f7f35fd5d5f8836
Leandro <fahurleandro@gmail.com>
2021-08-19 20:34:57 -0700

    update

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    --black: #000;
+    --white: #FFF;
+    --dark-gray:#333;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+    color: var(--black);    
+  }
+  html, body, #root {  
+    max-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;    
+    background-color: var(--white);
+  }
+`;
